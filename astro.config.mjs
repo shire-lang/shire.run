031820919065b2fce7287ef8fc221408519b0a35
CGQAQ <m.jason.liu@outlook.com>
2024-08-28 10:01:00 +0800

    feat: config deploy target

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'http://cloud.shire.run/',
+});
