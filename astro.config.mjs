ad30b6101dfdace2d3fd35f5575409581ac404fd
Phodal Huang <h@phodal.com>
2024-08-28 15:42:06 +0800

    refactor: rename domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://cloud.shire.run/',
+  site: 'http://shire.run/',
   integrations: [tailwind()]
 });
