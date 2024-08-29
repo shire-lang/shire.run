9d1f3cd2d539051f2cf7686e4f96b208ab0f6d11
Phodal Huang <h@phodal.com>
2024-08-29 20:54:28 +0800

    feat(markdown): add Shire language support for syntax highlighting
    Add support for the Shire language in markdown syntax highlighting configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,23 @@ import svelte from "@astrojs/svelte";
 
 import mdx from "@astrojs/mdx";
 
+import { bundledLanguages } from 'shiki'
+import shireLang from './src/shire.tmLanguage.json' assert { type: 'json' }
+
 // https://astro.build/config
 export default defineConfig({
   site: 'http://shire.run/',
-  markdown: { shikiConfig: { theme: 'css-variables' } },
+  markdown: { shikiConfig: {
+    theme: 'css-variables',
+      langs: [
+        ...Object.keys(bundledLanguages),
+        {
+          id: 'shire',
+          scopeName: 'source.shire',
+          aliases: ['caddy'],
+          ...shireLang
+        },
+      ],
+  } },
   integrations: [tailwind(), svelte(), mdx()]
 });
