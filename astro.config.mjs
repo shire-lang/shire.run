eed55204d5ae0361f7bf9f3e34f265536d9405d5
CGQAQ <m.jason.liu@outlook.com>
2024-09-02 14:39:14 +0800

    feat: 代码片段复制按钮

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,30 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 import mdx from "@astrojs/mdx";
+import astroExpressiveCode from "astro-expressive-code";
 
-import { bundledLanguages } from 'shiki'
-import shireLang from './src/shire.tmLanguage.json' assert { type: 'json' }
+import { bundledLanguages } from "shiki";
+import shireLang from "./src/shire.tmLanguage.json";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://shire.run/",
-  markdown: {
-    shikiConfig: {
-      theme: "css-variables",
-      langs: [
-        ...Object.keys(bundledLanguages),
-        {
-          id: "shire",
-          scopeName: "source.shire",
-          ...shireLang,
-        },
-      ],
-    },
-  },
-  integrations: [tailwind(), svelte(), mdx()],
+  integrations: [
+    tailwind(),
+    astroExpressiveCode({
+      shiki: {
+        theme: "css-variables",
+        langs: [
+          ...Object.keys(bundledLanguages),
+          {
+            id: "shire",
+            scopeName: "source.shire",
+            ...shireLang,
+          },
+        ],
+      },
+    }),
+    svelte(),
+    mdx(),
+  ],
 });
